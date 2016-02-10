8d9398cb4dabc42b8f71d4d151a83fed9270fc9e
kounelios13 <pkounelios@gmail.com>
2016-02-10 14:42:38 +0200

    first items commited

---
+++ first game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerController : MonoBehaviour {
+	public float speed=1;
+	private Rigidbody rb;
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+		rb.AddForce(movement * speed);
+	}
+	void Update () {
+	
+	}
+}
